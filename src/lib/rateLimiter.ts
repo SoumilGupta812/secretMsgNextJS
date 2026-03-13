9e3395d048e19813d10b38d202bc19eb06224085
blac-coffee <soumilgupta.812@gmail.com>
2026-03-13 23:32:39 +0530

    changes in rate limiting

--- src/lib/rateLimiter.ts
+++ src/lib/rateLimiter.ts
@@ -3,12 +3,12 @@ import { redis } from "./redis";
 
 export const apiLimiter = new Ratelimit({
   redis: redis,
-  limiter: Ratelimit.slidingWindow(5, "1 m"),
+  limiter: Ratelimit.slidingWindow(5, "1 h"),
   prefix: "api-rate-limit",
 });
 
 export const loginLimiter = new Ratelimit({
   redis: redis,
-  limiter: Ratelimit.slidingWindow(5, "1 m"),
+  limiter: Ratelimit.slidingWindow(5, "1 h"),
   prefix: "login-rate-limit",
 });
